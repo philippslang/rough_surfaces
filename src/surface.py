505fad4bd13224b3d8fb42900e95b13806d144a2
philipp <philipp.s.lang@gmail.com>
2016-05-09 10:35:18 +0100

    surface first version

---
+++ src/surface.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+
+class Surface:
+    '''
+    One- or two-dimensional surface height representation. 
+    
+    The assumption upon which this framework is based is a uniform lattice size in both directions, 
+    which is reflected in this class. 'Surface' is the fundamental class that most modules build 
+    upon. It usually represents the model or computational domain, as it may represent both,
+    individual or composite surfaces, i.e., rough surfaces or an aperture field.
+    
+    Standard initialization is from two-dimensional ndarray and lattice size:
+    >>> import numpy as np
+    >>> N, dxy = 100, 0.1
+    >>> h = np.zeros((N,N))
+    >>> s = Surface(h, dxy)
+    >>> s
+    Surface((100x100), dxy=0.1)
+    >>> s.length()  # egde length in x-direction
+    10.0
+    >>> s.length(1) # egde length in y-direction
+    10.0
+    
+    Surfaces can also be one-dimensional, e.g., represent traces or cross-sections:
+    >>> import numpy as np
+    >>> N, dxy = 100, 0.1
+    >>> h = np.zeros((N))
+    >>> s = Surface(h, dxy)
+    >>> s
+    Surface((100), dxy=0.1)
+    >>> s.length() # length
+    10.0
+    >>> s.length(1) # there is no second axis for one-dimensional surfaces
+    Traceback (most recent call last):
+        ...
+    IndexError: tuple index out of range
+    '''
+    
+    def __init__(self, input_array, dxy):
+        '''Input array [L] is copied to ndarray, and dxy [L] is cell size.'''
+        self.h = np.array(input_array)
+        self.dxy = float(dxy)
+
+    def __repr__(self):
+        cls_name = type(self).__name__
+        msg_shape = '{!r}x{!r}' if len(self.h.shape) == 2 else '{!r}'
+        msg = '{}((' + msg_shape + '), dxy={!r})'
+        return msg.format(cls_name, *self.h.shape, self.dxy)
+    
+    def __str__(self):
+        return '\n'.join([self.__repr__(), str(self.h)]) 
+    
+    def __eq__(self, other):
+        '''Hard comparison (no tol), first cheap lattice size, then element wise.'''
+        if not self.dxy == other.dxy:
+            return False
+        return np.equal(self.h, other.h).all()
+    
+    def __getitem__(self, pos):
+        return self.h[pos]
+    
+    def __setitem__(self, pos, val):
+        self.h[pos] = val
+    
+    def save(self, file):    
+        np.savez(file, h=self.h, dxy=self.dxy)
+        
+    @classmethod
+    def load(cls, file):
+        d = np.load(file)
+        return cls(d['h'], d['dxy'])
+    
+    @property
+    def shape(self):
+        return self.h.shape
+    
+    def rms(self):
+        '''Returns root-mean-square roughness [L].'''
+        return np.sqrt(np.mean(self.h**2))
+    
+    def length(self, axis=0):
+        '''Returns length [L] of surface in x- or y-direction, for axis=0 and 1, respectively.'''
+        return self.shape[axis]*self.dxy
+    
+    def nominal_area(self):
+        '''Returns length() [L] for 1D, area [L^2] for 2D.'''        
+        a = 1.0
+        for i in range(len(self.shape)):
+            a *= self.length(i)
+        return a
+    
+    def shift_to_zero_mean(self):
+        '''Shifts surface such that <h> = 0.'''
+        self.h = self.h-np.mean(self.h)
+        
+    def mean_aperture(self):
+        '''Composite surface assumption: mean of difference field to highest point.'''
+        return np.mean(np.abs(np.subtract(self.h, np.max(self.h))))
+    
+    def pore_volume(self):
+        '''Composite surface assumption: mean aperture times area (2D-->[L^3]) or length (1D-->[L^2]).'''
+        return self.mean_aperture()*self.nominal_area()
+
+    def scale_to_property(self, prop, target_value):
+        '''
+        Scales height to fit target property, which must be name of scalar returning method.
+        
+        >>> import numpy as np
+        >>> N, dxy = 100, 0.1
+        >>> h = np.ones((N))
+        >>> s = Surface(h, dxy)
+        >>> s.rms()
+        1.0
+        >>> s.scale_to_property('rms', 0.5)
+        >>> s.rms()
+        0.5
+        '''
+        is_value = getattr(self, prop)()        
+        self.h *= target_value/is_value
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ src/test_surface.py
@@ -0,0 +1,58 @@
+import unittest
+import numpy as np
+import surface as sr
+
+class TestSurface(unittest.TestCase):
+
+    def setUp(self):
+        self.N, self.dxy, self.val_init = 100, 1.0, 1.0
+        self.L = self.dxy*self.N        
+        
+    def _test_common(self, s):
+        assert(self.val_init == s.rms())
+        s.shift_to_zero_mean()
+        assert(s.rms() == 0.)
+        fname = '/tmp/save_surface.npz'
+        s.save(fname)
+        sclone = sr.Surface.load(fname)
+        assert(type(s) == type(sclone))
+        assert(s == sclone)
+        with self.assertRaises(AttributeError):
+            s.shape = 10
+
+    def test_interface_2D(self):
+        a = np.full((self.N,self.N), self.val_init)
+        s = sr.Surface(a, self.dxy)
+        assert(s[0,0] == self.val_init)
+        assert(s[0,self.N-1] == self.val_init)
+        with self.assertRaises(IndexError):
+            s[0,self.N]
+        new_val = 2.0
+        s[0,self.N-1] = new_val
+        assert(s[0,self.N-1] == new_val)
+        s[0,self.N-1] = self.val_init
+        assert(s.dxy == self.dxy)
+        assert(s.length() == self.L)
+        assert(s.length(1) == self.L)
+        assert(s.nominal_area() == self.L**2) 
+        self._test_common(s)
+        
+    def test_interface_1D(self):
+        a = np.full((self.N), self.val_init)
+        s = sr.Surface(a, self.dxy)        
+        assert(s.dxy == self.dxy)
+        assert(s.length() == self.L)
+        assert(s.nominal_area() == self.L)
+        assert(s[0] == self.val_init)
+        new_val = 2.0
+        s[0] = new_val
+        assert(s[0] == new_val)
+        s[0] = self.val_init
+        with self.assertRaises(IndexError):
+            s.length(1)
+        self._test_common(s)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
