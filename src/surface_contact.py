611f63d0c6162c5ae6ba027fef85d4f76c7c3b31
philipp <philipp.s.lang@gmail.com>
2016-07-20 17:39:11 +0100

    contact...

--- src/surface_contact.py
+++ src/surface_contact.py
@@ -1,4 +1,46 @@
+import numpy as np
 
+
+def homogeneous_composite_modulus(E, nu):
+    """Returns elastic composite modulus for homogeneous system, i.e. both sides of fracture of same material."""
+    return 1./( 2.*((1.-nu**2)/(E)))
+
+
+class Results:
+    """Elastic contact results, pressure and displacement field."""
+    
+    def __init__(self):
+        self.p = None
+        self.u = None
+        
+    def contact_area(self, dxy):
+        """Absolute area of contact in [L2], assuming uniform grid spacing."""
+        return len(self.p[self.p>0.])*dxy**2
+    
+    
+def _recon_FFT(x, y):
+    """
+    Internal method. Aides the convolution with the log of radius function.
+    
+    Parameters
+    ----------
+    x: array_like
+        A dense 2D array filled with x coords.
+    y: array_like
+        A dense 2D array filled with y coords.
+    
+    Returns
+    -------
+    reco: array_like
+        The convolution with the log of radius function.
+        
+    Notes
+    -----
+    Used in stiffness matrix computation of CG/FFT based contact algorithms.
+    """
+    r = np.sqrt(x*x+y*y)
+    reco = y*np.log(x+r) + x*np.log(y+r)
+    return reco
 
 
 if __name__ == '__main__':
--- src/test_surface_contact.py
+++ src/test_surface_contact.py
@@ -1,16 +1,36 @@
 import unittest
 import numpy as np
-import surface as sr
+import surface_generation as sg
+import surface_contact as sc
+
 
 class TestSurfaceContact(unittest.TestCase):
 
     def setUp(self):
-        self.N, self.dxy, self.val_init = 100, 1.0, 1.0
-        self.L = self.dxy*self.N        
+        self.F = [1.0E5]
+        self.E = 1.0E+9
+        self.nu = 0.3
+        self.radius = 0.5 
+        self.edge_length = 2.4*self.radius
+        self.N = 256        
 
     def test_hertz(self):
-        assert(1 == 1)
-        
+        s = sg.sphere(self.N, self.edge_length, self.radius, 2.)        
+        composite_radius = 1./(2.*(1./self.radius))
+        composite_modulus = sc.homogeneous_composite_modulus(self.E, self.nu)
+        for force in self.F:
+            # Hertz solution
+            contact_radius = (3*force*composite_radius/(4*composite_modulus))**(1/3)
+            approach = contact_radius**2/composite_radius
+            p_max = 3*force/(2*np.pi*contact_radius**2)
+            # numerical solution
+            contact_radius_num = np.sqrt(contact.contact_area(s.dxy)/np.pi)
+            approach_num = np.max(contact.u)
+            p_max_num = np.max(contact.p)
+            # check
+            np.isclose(contact_radius, contact_radius_num)
+            np.isclose(approach, approach_num)
+            np.isclose(p_max, p_max_num)
 
 
 if __name__ == '__main__':
