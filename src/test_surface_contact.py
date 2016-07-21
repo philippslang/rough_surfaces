db62d3346379a9e81a094c50635d484d4aacd1f7
philipp <philipp.s.lang@gmail.com>
2016-07-21 10:32:20 +0100

    contact

--- src/test_surface_contact.py
+++ src/test_surface_contact.py
@@ -14,12 +14,12 @@ def _rel_err(num, ref):
 class TestSurfaceContact(unittest.TestCase):
 
     def setUp(self):
-        self.F = [1.0E5,1.0E6,1.0E7]
+        self.F = [1.0E5,1.0E6]
         self.E = 1.0E+9
         self.nu = 0.3
         self.radius = 0.5 
         self.edge_length = 2.4*self.radius
-        self.N = 1024     
+        self.N = 300     
 
     def test_hertz(self):
         s = sg.sphere(self.N, self.edge_length, self.radius, 2.)        
@@ -32,19 +32,18 @@ class TestSurfaceContact(unittest.TestCase):
             p_max = 3*force/(2*np.pi*contact_radius**2)
             # numerical solution
             nominal_stress = force/self.edge_length**2
-            print(nominal_stress/1.0E6)
             contact = sc.contact_FFT(s, nominal_stress, self.E, self.nu, verbose=verbose)
             contact_radius_num = np.sqrt(contact.contact_area(s.dxy)/np.pi)
             approach_num = np.max(contact.u)
             p_max_num = np.max(contact.p)
             # check
             if verbose:
-                print(_rel_err(contact_radius_num, contact_radius))
-                print(_rel_err(approach_num, approach))
-                print(_rel_err(p_max_num, p_max))
-            assert(np.isclose(contact_radius, contact_radius_num, rtol=0.01))
-            assert(np.isclose(approach, approach_num, rtol=0.01))
-            assert(np.isclose(p_max, p_max_num, rtol=0.01))
+                print('Relative error radius = {:4.1f}%'.format(100*_rel_err(contact_radius_num, contact_radius)))
+                print('Relative error approach = {:4.1f}%'.format(100*_rel_err(approach_num, approach)))
+                print('Relative error pressure = {:4.1f}%'.format(100*_rel_err(p_max_num, p_max)))
+            assert(np.isclose(contact_radius_num, contact_radius, rtol=0.01))
+            assert(np.isclose(approach_num, approach, rtol=0.01))
+            assert(np.isclose(p_max_num, p_max, rtol=0.01))
 
 
 if __name__ == '__main__':
